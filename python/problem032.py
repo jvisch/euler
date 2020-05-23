a75e6ef606657cf26f73f9126c79e9e08303334f
Jorg Visch <jorgvisch@gmail.com>
2020-05-23 18:20:55 +0200

    typo' s and  main function added

--- python/problem032.py
+++ python/problem032.py
@@ -7,17 +7,18 @@ def is_pandigital(value):
     return True
 
 
-result = set()  # a set, don't count same products twice....
-for a in range(1, 100000):
-    for b in range(a, 10000):
-        product = a*b
-        # string to check
-        to_check = str(a)+str(b)+str(product)
-        if len(to_check) > 9:  # if total as above max lenght (it will never be shorter)
-            break
-        if is_pandigital(to_check):
-            print("{} x {} = {}".format(a, b, product))
-            result.add(product)
+def solve():
+    result = set()  # a set, don't count same products twice....
+    for a in range(1, 100000):
+        for b in range(a, 10000):
+            product = a*b
+            # string to check
+            to_check = str(a)+str(b)+str(product)
+            if len(to_check) > 9:  # if total is above max lenght (it will never be shorter)
+                break
+            if is_pandigital(to_check):
+                print("{} x {} = {}".format(a, b, product))
+                result.add(product)
+    return sum(result)
 
-print()
-print("result: {}".format(sum(result)))
+print(solve())
