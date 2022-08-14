2a33dc0d13e6a421cf1691c0b1bfe3d0b2aec50d
Jorg Visch <jorgvisch@gmail.com>
2022-08-14 13:13:46 +0200

    file path changed

--- python/problem022.py
+++ python/problem022.py
@@ -1,14 +1,12 @@
 from urllib.request import urlopen
 
-# with open(r'C:\Users\owwwt\Dropbox\projectEuler\python\p022_names.txt') as f:
+# with open(r'p022_names.txt') as f:
 #    names = f.read().replace('"', '').split(',')
 
 with urlopen('https://projecteuler.net/project/resources/p022_names.txt') as story:
     names = []
     for line in story:
         names.extend(line.decode('utf-8').replace('"', '').split(','))
-
-# names = names[1:10]
 
 names.sort()
 
