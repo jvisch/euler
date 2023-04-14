659a1cfe07d748781c5a1d70cffbd9047a61f55e
Jorg Visch <jorgvisch@gmail.com>
2023-04-14 09:02:23 +0200

    p 48

---
+++ python/p048.py
@@ -0,0 +1,9 @@
+
+def result():
+    powers = (i**i for i in range(1,1000))
+    total = sum(powers)
+    last_10_chars = str(total)[-10:]
+    return last_10_chars
+
+if __name__ == '__main__':
+    print(result())
