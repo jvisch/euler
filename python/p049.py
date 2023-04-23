81852922b0e85a07552ffe55375f511c4e7a93f7
Jorg Visch <jorgvisch@gmail.com>
2023-04-23 11:44:50 +0200

    problem 49

---
+++ python/p049.py
@@ -0,0 +1,40 @@
+import p046
+import p041
+import itertools
+
+
+def primes():
+    primes = p046.Primes.primes()
+    p = next(primes)
+    while p < 10000:
+        if p > 1000:
+            print(p)
+            yield p
+        p = next(primes)
+
+
+def are_permutations(numbers):
+    iter_numbers = iter(numbers)
+    first = next(iter_numbers)
+    key = sorted(p041.to_digits(first))
+    for i in iter_numbers:
+        if not key == sorted(p041.to_digits(i)):
+            return False
+    # All checks true
+    return True
+
+
+VALUE = 3330
+# all primes + 3330 , + 2 x 3330
+result = [(p, p + VALUE, p + VALUE + VALUE) for p in primes()]
+# filter none primes
+result = ((p1, p2, p3) for p1, p2, p3 in result if p046.Primes.is_prime(p2) and p046.Primes.is_prime(p3))
+# check for permutation
+result = (p for p in result if are_permutations(p))
+
+
+for r in result:
+    print(r)
+    all_digits = [d for n in r for d in p041.to_digits(n)]
+    the_real_result = p041.from_digits(all_digits)
+    print(the_real_result)
